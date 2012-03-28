d87ffa3ac377422dfaf484a0fd8858e2efa3b496
Takeutch Kemeco <takeutchkemeco@gmail.com>
2012-03-29 00:27:21 +0900

    gimp

---
+++ gimp/all-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+base_dir=$(pwd)
+
+for package in $(ls)
+do
+  cd $base_dir/$package
+  git pull
+  ./autogen.sh --prefix=/usr --disable-python
+  make clean
+  make
+  make install
+  ldconfig
+  cd $base_dir
+done
+
