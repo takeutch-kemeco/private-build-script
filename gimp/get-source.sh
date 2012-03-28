41f9ba2748cefd76d58e7dbd1093b78a7f055f51
Takeutch Kemeco <takeutchkemeco@gmail.com>
2012-03-29 00:33:24 +0900

    gimp

---
+++ gimp/get-source.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone git://git.gnome.org/babl
+git clone git://git.gnome.org/gegl
+git clone git://git.gnome.org/gimp
+git clone git://git.gnome.org/gssdp
+git clone git://git.gnome.org/gupnp-av
+git clone git://git.gnome.org/gupnp-dlna
+git clone git://git.gnome.org/gupnp-vala
+git clone git://git.gnome.org/gupnp
+git clone git://git.gnome.org/vala
+
