f375e6a7d4b7ac03f9438332911688beb90ab03c
Takeutch Kemeco <takeutchkemeco@gmail.com>
2012-06-11 02:35:06 +0900

    glibc

--- glibc/all-build.sh
+++ glibc/all-build.sh
@@ -9,13 +9,21 @@ glibc() {
 	cd $BASE_DIR/build-glibc
 
 cat > configparms << "EOF"
-ASFLAGS-config=-march=core2 -mtune=core2 -msse3 -Wa,--noexecstack
+ASFLAGS-config=-march=core2 -Wa,--noexecstack
 EOF
 	$BASE_DIR/glibc/configure --prefix=$PREFIX \
 		--enable-kernel=3.1 \
-		--libexecdir=$PREFIX/lib/glibc
+		--libexecdir=$PREFIX/lib/glibc \
+		--disable-profile \
+		--enable-add-ons
+
 	make
 	make install
+
+	cp -v $BASE_DIR/glibc/sunrpc/rpc/*.h $PREFIX/include/rpc/
+	cp -v $BASE_DIR/glibc/sunrpc/rpcsvc/*.h $PREFIX/include/rpc/
+	cp -v $BASE_DIR/glibc/nis/rpcsvc/*.h $PREFIX/include/rpc/
+
 	ldconfig
 
 	cd $BASE_DIR
