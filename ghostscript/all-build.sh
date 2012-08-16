c61b0cd4540c728a2caa285124fea2202aa35eba
Takeutch Kemeco <takeutchkemeco@gmail.com>
2012-08-16 23:53:21 +0900

    ghostscript

--- ghostscript/all-build.sh
+++ ghostscript/all-build.sh
@@ -3,25 +3,25 @@
 BASE_DIR=$(pwd)
 PREFIX=/usr
 
-MAKE_CLEAN="make clean"
+MAKE_CLEAN="__mk clean"
 IPA_FONT_DIR="/usr/share/fonts/IPAfont00303"
+
+. ../common-func/__common-func.sh
 
 lcms()
 {
-	cd $BASE_DIR/Little-CMS
+	__cd $BASE_DIR/Little-CMS
 	autoreconf -vfi
 	./configure --prefix=$PREFIX
 	$MAKE_CLEAN
-	make
-	make install
+	__mk
+	__mk install
 	ldconfig
-
-	cd $BASE_DIR
 }
 
 ghostscript()
 {
-        cd $BASE_DIR/ghostpdl/gs
+        __cd $BASE_DIR/ghostpdl/gs
 
 cat > $BASE_DIR/ghostpdl/gs/Resource/Init/cidfmap << "EOF"
 /Adobe-Japan1 << /FileType /TrueType /Path (ipag.ttf) /CSI [(Japan1) 6] >> ;
@@ -29,18 +29,16 @@ EOF
 
         ./autogen.sh --prefix=$PREFIX
         $MAKE_CLEAN
-        make
-        make install
-	make so
-	make soinstall
+        __mk
+        __mk install
+	__mk so
+	__mk soinstall
         ldconfig
 
 	mkdir -p $PREFIX/share/ghostscript/fonts
 	cp $IPA_FONT_DIR/*.ttf $PREFIX/share/ghostscript/fonts
-
-        cd $BASE_DIR
 }
 
-#lcms
+lcms
 ghostscript
 
