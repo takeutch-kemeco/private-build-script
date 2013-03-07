bba7ec3fb33a5cb9ada96a7711d53486b6b4d0cf
Takeutch Kemeco <takeutchkemeco@gmail.com>
2013-03-07 17:26:48 +0900

    update xfce4

--- xfce4/all-build.sh
+++ xfce4/all-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BASE_DIR=$(pwd)
-__X11_LIB__=/usr/X11/lib
+SRC_DIR=${BASE_DIR}/src
 
 #DIST_CLEAN=
 DIST_CLEAN="make distclean"
@@ -9,17 +9,35 @@ DIST_CLEAN="make distclean"
 #MAKE_CLEAN=
 MAKE_CLEAN="make clean"
 
-. ../common-func/__common-func.sh
+. ./__common-func.sh
 
-__common()
+__init-env()
 {
-	__cd $1
+	echo
+}
+
+__git-clean()
+{
+	git clone . b
+	cp b/* . -rf
+	rm b -rf
+	git checkout master
+	git pull
+}
+
+__bld-common()
+{
+	__git-clean
+
+	cp configure.ac.in{,.orig}
+	sed -e "s/-Wall/ /g" configure.ac.in.orig | sed -e "s/-Werror/ /g" > configure.ac.in
 
 	$DIST_CLEAN
-	./autogen.sh --prefix=/usr			\
-		--sysconfdir=/etc			\
-		--enable-maintainer-mode 		\
-		--enable-debug=no			\
+	CFLAGS="-O4 -D_FORTIFY_SOURCE=0 -D__OPTIMIZE__" CXXFLAGS="-O4"		\
+	./autogen.sh --prefix=/usr		\
+	      	     --sysconfdir=/etc		\
+	             --enable-maintainer-mode	\
+		     $@
 
 	$MAKE_CLEAN
 	__mk
@@ -27,21 +45,22 @@ __common()
 	ldconfig
 }
 
-__xfce4-dev-tools()
+__common()
 {
-	__common $BASE_DIR/xfce4-dev-tools
+	__cd $1
+	__bld-common
 }
 
 __vte()
 {
-	__cd $BASE_DIR/vte
+	__cd vte
+	__git-clean
 
 	$DIST_CLEAN
 	./autogen.sh --prefix=/usr 			\
-		--enable-maintainer-mode 		\
-		--enable-debug=no 			\
-		--enable-vala=yes 			\
-		--enable-introspection=yes		\
+	             --sysconfdir=/etc			\
+	             --libexecdir=/usr/lib/vte-2.90	\
+	             --enable-introspection=yes
 
 	$MAKE_CLEAN
 	__mk
@@ -51,188 +70,137 @@ __vte()
 
 __libxfce4util()
 {
-	__common $BASE_DIR/libxfce4util
+	__common libxfce4util
 }
 
 __xfconf()
 {
-	__common $BASE_DIR/xfconf
+	__common xfconf
 }
 
 __libxfce4ui()
 {
-	__common $BASE_DIR/libxfce4ui
+	__common libxfce4ui
 }
 
 __exo()
 {
-	__cd $BASE_DIR/exo
-
-	$DIST_CLEAN
-	./autogen.sh --prefix=/usr			\
-		--enable-maintainer-mode 		\
-		--enable-debug=no 			\
-		--sysconfdir=/etc 			\
-		--docdir=/usr/share/doc/exo		\
-
-	$MAKE_CLEAN
-	__mk
-	__mk install
-	ldconfig
-}
-
-__garcon()
-{
-	__common $BASE_DIR/garcon
-}
-
-__gtk-xfce-engine()
-{
-       __common $BASE_DIR/gtk-xfce-engine
-}
-
-__libwnck()
-{
-       __cd $BASE_DIR/libwnck
-
-	$DIST_CLEAN
-        ./autogen.sh --prefix=/usr			\
-                --enable-maintainer-mode 		\
-                --enable-debug=no 			\
-		--program-suffix=-1			\
-
-        $MAKE_CLEAN
-	__mk GETTEXT_PACKAGE=libwnck-1
-        __mk install
-        ldconfig
-}
-
-__libglade()
-{
-        __common $BASE_DIR/libglade
-}
-
-__libxfcegui4()
-{
-	__common $BASE_DIR/libxfcegui4
+	__common exo
 }
 
 __thunar()
 {
-	__common $BASE_DIR/thunar
+	__common thunar
+}
+
+__garcon()
+{
+	__common garcon
 }
 
 __thunar-volman()
 {
-	__common $BASE_DIR/thunar-volman
+	__common thunar-volman
 }
 
 __tumbler()
 {
-	__common $BASE_DIR/tumbler
+	__common tumbler
 }
 
 __xfce4-appfinder()
 {
-        __common $BASE_DIR/xfce4-appfinder
+        __common xfce4-appfinder
+}
+
+__xfce4-dev-tools()
+{
+	__common xfce4-dev-tools
 }
 
 __xfce4-panel()
 {
-	__cd $BASE_DIR/xfce4-panel
-
-	$DIST_CLEAN
-	./autogen.sh --prefix=/usr 			\
-		--enable-maintainer-mode 		\
-		--enable-debug=no 			\
-		--sysconfdir=/etc 			\
-		--docdir=/usr/share/doc/xfce4-panel	\
-
-	$MAKE_CLEAN
-	__mk
-	__mk install
-	ldconfig
+	__common xfce4-panel
 }
 
 __xfce4-power-manager()
 {
-	__common $BASE_DIR/xfce4-power-manager
+	__common xfce4-power-manager
 }
 
 __xfce4-session()
 {
-	__cd $BASE_DIR/xfce4-session
-
-	$DIST_CLEAN
-	./autogen.sh --prefix=/usr 			\
-		--enable-maintainer-mode 		\
-		--enable-debug=no 			\
-		--sysconfdir=/etc 			\
-		--docdir=/usr/share/doc/xfce4-session	\
-
-	$MAKE_CLEAN
-	__mk
-	__mk install
-	ldconfig
+	__common xfce4-session
 }
 
 __xfce4-settings()
 {
-	__common $BASE_DIR/xfce4-settings
+	__common xfce4-settings
 }
 
 __xfdesktop()
 {
-	__common $BASE_DIR/xfdesktop
+	__common xfdesktop
 }
 
 __xfwm4()
 {
-	__common $BASE_DIR/xfwm4
+	__common xfwm4
+}
+
+__xfce4-taskmanager()
+{
+	__common xfce4-taskmanager
 }
 
 __terminal()
 {
-	__cd $BASE_DIR/terminal
+	__common terminal
+}
 
-	$DIST_CLEAN
-	./autogen.sh --prefix=/usr 			\
-		--enable-maintainer-mode 		\
-		--enable-debug=no 			\
-		--docdir=/usr/share/doc/terminal	\
+__ristretto()
+{
+	__common ristretto
+}
 
-	$MAKE_CLEAN
-	__mk
-	__mk install
-	ldconfig
+__base-system()
+{
+#	__rem() {
+	__vte
+	__libxfce4util
+	__xfconf
+	__libxfce4ui
+	__exo
+	__thunar
+	__garcon
+	__gtk-xfce-engine
+###	__thunar-volman # required gudev
+	__tumbler
+	__xfce4-appfinder
+	__xfce4-dev-tools
+	__xfce4-panel
+###	__xfce4-power-manager # required libnotify
+	__xfce4-session
+	__xfce4-settings
+	__xfdesktop
+	__xfwm4
+}
+
+__extra-apps()
+{
+#	__rem() {
+	__xfce4-taskmanager
+	__terminal
+	__ristretto
 }
 
 __all()
 {
-__rem(){
-__xfce4-dev-tools
-__vte
-__libxfce4util
-__xfconf
-__libxfce4ui
-__exo
-__garcon
-__gtk-xfce-engine
-__libglade
-__libxfcegui4
-__thunar
-__thunar-volman
-__tumbler
-__xfce4-appfinder
-__xfce4-panel
-}
-__xfce4-power-manager
-__xfce4-session
-__xfce4-settings
-__xfdesktop
-__xfwm4
-__terminal
+#	__rem() {
+	__base-system
+	__extra-apps
 }
 
+__init-env
 $@
 
