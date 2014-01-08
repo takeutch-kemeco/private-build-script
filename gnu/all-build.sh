1cd05df8405d7b4b351fe5f9c4f6f3f87e5599c3
Takeutch Kemeco <takeutchkemeco@gmail.com>
2014-01-09 06:27:53 +0900

    refact: gnu

--- gnu/all-build.sh
+++ gnu/all-build.sh
@@ -1,56 +1,40 @@
 #!/bin/bash
 
 BASE_DIR=$(pwd)
-PREFIX=/usr
 
-#DIST_CLEAN=
-DIST_CLEAN="make distclean"
+DIST_CLEAN=
+#DIST_CLEAN="make distclean"
 
-#MAKE_CLEAN=
-MAKE_CLEAN="make clean"
+MAKE_CLEAN=
+#MAKE_CLEAN="make clean"
 
-. ../common-func/__common-func.sh
-
-__common()
-{
-	__cd $1
-
-	$DIST_CLEAN
-	./autogen.sh
-	./configure --prefix=/usr --sysconfdir=/etc
-
-	$MAKE_CLEAN
-	__mk
-	__mk install
-	ldconfig
-}
+. ../common-func/__common-func-2.sh
 
 __patch()
 {
-	__cd $BASE_DIR/patch
-	./bootstrup
-
-	__common $BASE_DIR/patch
+    __cd patch
+    ./bootstrup
+    __bld-common
 }
 
 __libunistring()
 {
-	__common $BASE_DIR/libunistring
+    __common libunistring
 }
 
 __guile()
 {
-	__common $BASE_DIR/guile
+    __common guile
 }
 
 __libidn()
 {
-	__common $BASE_DIR/libidn
+    __common libidn
 }
 
 __gnutls()
 {
-	__common $BASE_DIR/gnutls
+    __common gnutls
 }
 
 ### memo ###
@@ -58,8 +42,8 @@ __gnutls()
 # (tex環境が万全でない場合)
 # だが、エラーが出てもマニュアルもビルドに失敗してるだけなので、make install は通る。気にしなくていい。
 __gperf() {
-	__cd $BASE_DIR/gperf
-	cp configure.ac configure.ac.old
+    __cd gperf
+    cp configure.ac configure.ac.old
 
 echo "diff --git a/configure.ac b/configure.ac
 index 5fb4099..0315a6a 100644
@@ -84,23 +68,21 @@ index 5fb4099..0315a6a 100644
  }
  '" > configure.ac.patch
 
-	patch < configure.ac.patch
+    patch < configure.ac.patch
 
-	__common $BASE_DIR/gperf
-
-	__cd $BASE_DIR/gperf
-	rm configure.ac
-	cp configure.ac.old configure.ac
+    __bld-common
+    rm configure.ac
+    cp configure.ac.old configure.ac
 }
 
 __ed()
 {
-	__common $BASE_DIR/ed
+	__common ed
 }
 
 __attr()
 {
-	__cd $BASE_DIR/attr
+	__cd attr
 
 	$DIST_CLEAN
 	./autogen.sh
@@ -111,82 +93,72 @@ __attr()
 		--libexecdir=/usr/lib
 
 	__mk
-	__mk install install-dev install-lib
-	ldconfig
+	sudo make install install-dev install-lib
+	sudo ldconfig
 }
 
 __acl()
 {
-	__common $BASE_DIR/acl
+	__cd acl
 
 	$DIST_CLEAN
 	./autogen.sh
 	INSTALL_USER=root  \
 	INSTALL_GROUP=root \
-	./configure --prefix=$PREFIX	\
+	./configure --prefix=/usr	\
 		--libdir=/lib		\
 		--libexecdir=/usr/lib
 
 	__mk
-	__mk install install-dev install-lib
+	sudo make install install-dev install-lib
 
-	make install install-dev install-lib
-	chmod -v 0755 /lib/libacl.so.1.1.0
-	rm -v /lib/libacl.{a,la,so}
-	ln -sfv ../../lib/libacl.so.1 /usr/lib/libacl.so
-	sed -i "s|libdir='/lib'|libdir='/usr/lib'|" /usr/lib/libacl.la
-	install -v -m644 doc/*.txt /usr/share/doc/acl
+	sudo chmod -v 0755 /lib/libacl.so.1.1.0
+	sudo rm -v /lib/libacl.{a,la,so}
+	sudo ln -sfv ../../lib/libacl.so.1 /usr/lib/libacl.so
+	sudo sed -i "s|libdir='/lib'|libdir='/usr/lib'|" /usr/lib/libacl.la
+	sudo install -v -m644 doc/*.txt /usr/share/doc/acl
 
-	ldconfig
+	sudo ldconfig
 }
 
 __indent()
 {
-	__common $BASE_DIR/indent
+	__common indent
 }
 
 __expect()
 {
-	__cd $BASE_DIR/expect
-
-	$DIST_CLEAN
-	./configure --prefix=/usr	\
-            	--with-tcl=/usr/lib \
-            	--with-tclinclude=/usr/include \
-            	--enable-shared
-
-	$MAKE_CLEAN
-	__mk
-	__mk install
-	ldconfig
+	__cd expect
+        __bld-common --with-tcl=/usr/lib --with-tclinclude=/usr/include --enable-shared
 }
 
 __dejagnu()
 {
-	__common $BASE_DIR/dejagnu
+	__common dejagnu
 }
 
 __wget()
 {
-	__common $BASE_DIR/wget
+	__cd wget
+        __bld-common --with-ssl=gnutls --disable-ipv6
 }
 
 __all()
 {
 #__rem(){
-__patch
-__libunistring
-__guile
-__libidn
-__gnutls
-__ed
-__gperf
-__attr
-__acl
-__indent
-__expect
-__dejagnu
-__wget
+    __patch
+    __libunistring
+    __guile
+    __libidn
+    __gnutls
+    __ed
+    __gperf
+    __attr
+    __acl
+    __indent
+    __expect
+    __dejagnu
+    __wget
 }
 
 $@
