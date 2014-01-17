da2a254c73080e4ae2bfb87083bb20bebd41b227
Takeutch Kemeco <takeutchkemeco@gmail.com>
2014-01-17 20:23:02 +0900

    fix: gimp-2.8.10

--- gimp-2.8.10/all-build.sh
+++ gimp-2.8.10/all-build.sh
@@ -3,58 +3,40 @@
 BASE_DIR=$(pwd)
 SRC_DIR=${BASE_DIR}/src
 
-. ./__common-func.sh
+. ../common-func/__common-func-2.sh
 
 DIST_CLEAN=""
 #DIST_CLEAN="make distclean"
 
-#MAKE_CLEAN=""
-MAKE_CLEAN="make clean"
-
-__bld-common()
-{
-	$DIST_CLEAN
-	__cfg --prefix=/usr --sysconfdir=/etc $@ 
-
-	$MAKE_CLEAN
-	__mk
-	__mk install
-	ldconfig
-}
-
-__common()
-{
-	__cd $1
-	__bld-common
-}
+MAKE_CLEAN=""
+#MAKE_CLEAN="make clean"
 
 __babl()
 {
-	__wget ftp://ftp.gimp.org/pub/babl/0.1/babl-0.1.10.tar.bz2
-	__decord babl-0.1.10
-	__common babl-0.1.10
+    __wget ftp://ftp.gimp.org/pub/babl/0.1/babl-0.1.10.tar.bz2
+    __decord babl-0.1.10
+    __common babl-0.1.10
 }
 
 __gegl()
 {
-	__wget ftp://ftp.gimp.org/pub/gegl/0.2/gegl-0.2.0.tar.bz2
-	__dcd gegl-0.2.0
-	__bld-common --without-libavformat --without-libv4l --disable-docs --without-libjpeg
+    __wget ftp://ftp.gimp.org/pub/gegl/0.2/gegl-0.2.0.tar.bz2
+    __dcd gegl-0.2.0
+    __bld-common --without-libavformat --without-libv4l --disable-docs --without-libjpeg
 }
 
 __gimp()
 {
-	__wget http://www.ring.gr.jp/pub/graphics/gimp/v2.8/gimp-2.8.10.tar.bz2
-	__dcd gimp-2.8.10
-	patch -p1 < ../log_value_mode-gimp-2.8.10.patch
-	__bld-common --disable-python --without-alsa --without-gvfs --without-libjpeg
+    __wget http://www.ring.gr.jp/pub/graphics/gimp/v2.8/gimp-2.8.10.tar.bz2
+    __dcd gimp-2.8.10
+    patch -p1 < ${BASE_DIR}/log_value_mode-gimp-2.8.10.patch
+    __bld-common --disable-python --without-alsa --without-gvfs --without-libjpeg
 }
 
 __all() {
-#rem() {
-	__babl
-	__gegl
-	__gimp
+    __babl
+    __gegl
+    __gimp
 }
 
 $@
