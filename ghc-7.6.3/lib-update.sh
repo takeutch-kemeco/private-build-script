345c1b7beb6482c283e2b8cdedb3c6f99e778b2d
Takeutch Kemeco <takeutchkemeco@gmail.com>
2014-01-26 12:50:07 +0900

    fix: ghc-7.6.3

--- ghc-7.6.3/lib-update.sh
+++ ghc-7.6.3/lib-update.sh
@@ -3,11 +3,13 @@
 __cabal()
 {
     sudo cabal install --force-reinstall --reinstall \
+                       --enable-shared \
                        --enable-documentation --haddock-html \
                        --global --prefix=/usr $@
+    sudo ghc-pkg recache
 }
 
-sudo cabal update
+#sudo cabal update
 
 for __tmp in `ghc-pkg list`
 do
