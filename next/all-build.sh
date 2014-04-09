b831017a0c9f5095bcfe38707ee478bd4f0f3202
Takeutch Kemeco <takeutchkemeco@gmail.com>
2014-04-09 13:43:36 +0900

    update: next(webkitgtk)

--- next/all-build.sh
+++ next/all-build.sh
@@ -2020,18 +2020,18 @@ __vte()
     __bld-common --enable-introspection --enable-maintainer-mode
 }
 
-__webkitgtk-2.2.5()
+__webkitgtk-2.2.6()
 {
     __dep gperf gst-plugin-base gtk+3 icu libsecret libsoup libwebp mesalib ruby sqlite gudev which gobject-introspection
 
-    __wget http://webkitgtk.org/releases/webkitgtk-2.2.5.tar.xz
-    __dcd webkitgtk-2.2.5
+    __wget http://webkitgtk.org/releases/webkitgtk-2.2.6.tar.xz
+    __dcd webkitgtk-2.2.6
     __bld-common --enable-introspection --disable-geolocation --enable-webkit1=no
 }
 
 __webkitgtk()
 {
-    __webkitgtk-2.2.5
+    __webkitgtk-2.2.6
 }
 
 __wget-1.15()
