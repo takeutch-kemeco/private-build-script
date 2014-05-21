3b641fbe86d9802f7a4a519a865663e80b4e03b0
Takeutch Kemeco <takeutchkemeco@gmail.com>
2014-05-21 22:19:06 +0900

    fix: next(perl)

--- next/all-build.sh
+++ next/all-build.sh
@@ -2072,6 +2072,10 @@ __perl-5.8.2()
 
     __wget http://www.cpan.org/src/5.0/perl-5.18.2.tar.gz
     __dcd perl-5.18.2
+    sed -i -e "s|BUILD_ZLIB\s*= True|BUILD_ZLIB = False|" \
+	-e "s|INCLUDE\s*= ./zlib-src|INCLUDE    = /usr/include|" \
+	    -e "s|LIB\s*= ./zlib-src|LIB        = /usr/lib|" \
+	cpan/Compress-Raw-Zlib/config.in
     sh Configure -des -Dprefix=/usr -Dvendorprefix=/usr -Dman1dir=/usr/share/man/man1 -Dman3dir=/usr/share/man/man3 \
         -Dpager="/usr/bin/less -isR" -Duseshrplib
     __mk
