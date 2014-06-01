42dbf427bcc93adb8a80e23779e1475e6187c07f
Takeutch Kemeco <takeutchkemeco@gmail.com>
2014-06-01 10:02:46 +0900

    fix: emacs

--- emacs/all-build.sh
+++ emacs/all-build.sh
@@ -3,48 +3,44 @@
 BASE_DIR=$(pwd)
 SRC_DIR=$BASE_DIR/src
 
-MAKE_CLEAN="make clean"
-DIST_CLEAN="make distclean"
+MAKE_CLEAN=
+#MAKE_CLEAN="make clean"
 
-. __common-func.sh
+DIST_CLEAN=
+#DIST_CLEAN="make distclean"
+
+. ../common-func/__common-func-2.sh
+
+__emacs-24.3()
+{
+    __dep "?"
+
+    __wget http://core.ring.gr.jp/pub/GNU/emacs/emacs-24.3.tar.xz
+    __dcd emacs-24.3
+    __bld-common --localstatedir=/var --libexecdir=/usr/lib --without-gif --with-x-toolkit=yes
+}
 
 __emacs()
 {
-	__wget http://core.ring.gr.jp/pub/GNU/emacs/emacs-24.3.tar.xz
-	__dcd emacs-24.3
-
-	$DIST_CLEAN
-	__cfg --prefix=/usr				\
-	      --sysconfdir=/etc				\
-	      --localstatedir=/var			\
-	      --libexecdir=/usr/lib			\
-	      --without-gif                             \
-              --with-x-toolkit=yes
-
-	$MAKE_CLEAN
-	__mk
-	__mk install
-	ldconfig
+    __emacs-24.3
 }
 
 __haskell-mode()
 {
-	git clone git://github.com/haskell/haskell-mode.git
-	__cd haskell-mode
-	git pull
-
-	cp ../haskell-mode $PREFIX/lib/emacs/ -rf
+    __git-clone git://github.com/haskell/haskell-mode.git
+    __cd haskell-mode
+    __git-pull
+    sudo cp ../haskell-mode /usr/lib/emacs/ -rf
 }
 
 __haskell-mode-config()
 {
-	__mes haskell-mode-config
+    __mes haskell-mode-config
 
-grep "haskell-mode" /etc/emacs.el
-if [ $? -ne 0 ]
-then 
-cat >> /etc/emacs.el << "EOF"
-
+    grep "haskell-mode" /etc/emacs.el
+    if [ $? -ne 0 ]
+    then
+        cat >> /etc/emacs.el << .
 (load "/usr/lib/emacs/haskell-mode/haskell-site-file")
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 
@@ -53,17 +49,15 @@ cat >> /etc/emacs.el << "EOF"
 (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
 
 (setq haskell-program-name "/usr/bin/ghci")
-
-EOF
-fi
+.
+    fi
 }
 
 __all()
 {
-#__rem() {
-	__emacs
-	__haskell-mode
-	__haskell-mode-config
+    __emacs
+    __haskell-mode
+    __haskell-mode-config
 }
 
 $@
