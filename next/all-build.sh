48377026b1758d9b15013c5883227e46f37c1d56
Takeutch Kemeco <takeutchkemeco@gmail.com>
2015-04-27 00:52:37 +0900

    update: next(openssh)

--- next/all-build.sh
+++ next/all-build.sh
@@ -2804,13 +2804,13 @@ __openldap()
     __openldap-2.4.40
 }
 
-__openssh-6.7p1()
+__openssh-6.8p1()
 {
     __dep openssl linux-pam
 
-    __wget http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.7p1.tar.gz
-    __decord openssh-6.7p1
-    __cd openssh-6.7p1
+    __wget http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.8p1.tar.gz
+    __decord openssh-6.8p1
+    __cd openssh-6.8p1
 
     sudo install -v -m700 -d /var/lib/sshd
     sudo chown -v root:sys /var/lib/sshd
@@ -2827,13 +2827,13 @@ __openssh-6.7p1()
 
     sudo install -v -m755 contrib/ssh-copy-id /usr/bin
     sudo install -v -m644 contrib/ssh-copy-id.1 /usr/share/man/man1
-    sudo install -v -m755 -d /usr/share/doc/openssh-6.7p1
-    sudo install -v -m644 INSTALL LICENCE OVERVIEW README* /usr/share/doc/openssh-6.7p1
+    sudo install -v -m755 -d /usr/share/doc/openssh-6.8p1
+    sudo install -v -m644 INSTALL LICENCE OVERVIEW README* /usr/share/doc/openssh-6.8p1
 }
 
 __openssh()
 {
-    __openssh-6.7p1
+    __openssh-6.8p1
 }
 
 __openssl-1.0.2a()
